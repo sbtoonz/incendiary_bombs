f4264fe9eb0ba023273ecf4feb62020eb8023cfb
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-03-21 10:21:29 -0700

    sticky bomb: Polish

--- Incendiary_Bombs/Patches.cs
+++ Incendiary_Bombs/Patches.cs
@@ -6,44 +6,24 @@ namespace Incendiary_Bombs
 {
     public class Patches
     {
-        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
-        public static class ObjectDB_StatthingerPatch
+        [HarmonyPatch(typeof(Projectile), nameof(Projectile.OnHit))]
+        public static class OnHitPatch
         {
-            [HarmonyPostfix]
-            [UsedImplicitly]
-            public static void Postfix(ObjectDB __instance)
+            public static void Prefix(Projectile __instance)
             {
-                var go1 = __instance.GetItemPrefab("BombIncendiary");
-                var damtype = __instance.m_items.Find(x=>x.gameObject == go1).GetComponent<ItemDrop>().m_itemData.m_shared.m_damages;
-                damtype.m_damage =0;
-                damtype.m_blunt =0;
-                damtype.m_slash =0;
-                damtype.m_pierce =0;
-                damtype.m_chop =0;
-                damtype.m_pickaxe =0;
-                damtype.m_fire =0;
-                damtype.m_frost =0;
-                damtype.m_lightning =0;
-                damtype.m_poison =0;
-                damtype.m_spirit =0;
-                __instance.m_items.Find(x => x.gameObject == go1).GetComponent<ItemDrop>().m_itemData.m_shared
-                    .m_damages = damtype;
-                var go2 = __instance.GetItemPrefab("BombIncendiary_Sticky");
-                var damtype1 = __instance.m_items.Find(x=>x.gameObject == go2).GetComponent<ItemDrop>().m_itemData.m_shared.m_damages;
-                damtype1.m_damage =0;
-                damtype1.m_blunt =0;
-                damtype1.m_slash =0;
-                damtype1.m_pierce =0;
-                damtype1.m_chop =0;
-                damtype1.m_pickaxe =0;
-                damtype1.m_fire =0;
-                damtype1.m_frost =0;
-                damtype1.m_lightning =0;
-                damtype1.m_poison =0;
-                damtype1.m_spirit =0;
-                __instance.m_items.Find(x => x.gameObject == go2).GetComponent<ItemDrop>().m_itemData.m_shared
-                    .m_damages = damtype1;
-
+                if (__instance.gameObject.name.StartsWith("sticky_projectile"))
+                {
+                    __instance.m_aoe = 1;
+                }
+            }
+            public static void Postfix(Projectile __instance)
+            {
+                if (__instance.gameObject.name.StartsWith("sticky_projectile"))
+                {
+                    __instance.m_rotateVisual = 0;
+                    var transformLocalScale = __instance.m_visual.transform.localScale * (float)1.5;
+                    __instance.m_visual.transform.localScale = transformLocalScale;
+                }   
             }
         }
     }
